bc98429b20cf9145c667dabad41df4c8a5f7ec67
abransh <abranshbaliyan2807@gmail.com>
2025-02-18 18:07:34 +0100

    sign-in sign up page under (auth)

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-       <div className = "flex flex-col"> 
+       <div className = ""> 
     <p className = "text-3xl font-bold text-indigo-500" > 
     Yo bitches, Chatify gonna fuck y'all
      </p>
