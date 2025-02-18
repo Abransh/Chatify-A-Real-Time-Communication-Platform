dc6023003c8eed1e657278fe0b3d7174d19dc360
abransh <abranshbaliyan2807@gmail.com>
2025-02-18 17:35:56 +0100

    Added button component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <p className = "text-3xl font-bold text-indigo-500" > Yo bitches, Chatify gonna fuck y'all </p>
+       <div className = "flex flex-col"> 
+    <p className = "text-3xl font-bold text-indigo-500" > 
+    Yo bitches, Chatify gonna fuck y'all
+     </p>
+     <Button> 
+      click me
+     </Button>
+     </div>
   )
 
 }
