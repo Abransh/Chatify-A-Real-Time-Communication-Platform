6711aa5ca615c17521c99ad1f8bec97c96ad4348
abransh <abranshbaliyan2807@gmail.com>
2025-02-19 18:24:28 +0100

    added dialog from @components/ui/dialog

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -1,42 +1,29 @@
-"use client"; 
+"use client";
 
-export const InitialModal  = () => {
-        return 
-      (
-        <div>
-            <h1>Welcome to the Initial Modal</h1>
-        </div>
-      )
+import React from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle
+} from "@/components/ui/dialog";
 
-    }
 
-    export default InitialModal;
-// import {
-//     Dialog,
-//     DialogContent,
-//     DialogDescription,
-//     DialogFooter,
-//     DialogHeader,
-//     DialogTitle
-//   } from "@/components/ui/dialog";
-
-//   export const InitalModal  = () => {
-//     return 
-//   (
-   
-// <dialog open>
-//       <DialogContent className="bg-white text-black p-0 overflow-hidden">
-//         <DialogHeader className="pt-8 px-6">
-//           <DialogTitle className="text-2xl text-center font-bold">
-//             Customize your server
-//           </DialogTitle>
-//           <DialogDescription className="text-center text-zinc-500">
-//             Give your server a personality with a name and an image. You can
-//             always change it later.
-//           </DialogDescription>
-//         </DialogHeader>
-//        </DialogContent>
-//     </dialog>
-//   )
-
-// }
+export const InitialModal: React.FC = () => {
+    return (
+        <Dialog open> 
+         <DialogContent className="bg-white text-black p-0 overflow-hidden">
+            <DialogHeader className="pt-8 px-6">
+                <DialogTitle className="text-2xl text-center font-bold">
+                customize your server
+                    </DialogTitle>
+                    <DialogDescription className="text-center text-zinc-500">
+                        Give your server a name and image
+                    </DialogDescription>
+            </DialogHeader>
+          </DialogContent>
+         </Dialog>
+    );
+}
