fed5284f86779a579e6ead74d54a9364fba5483c
abransh <abranshbaliyan2807@gmail.com>
2025-02-19 02:49:01 +0100

    initializing-profile update

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,11 +1,23 @@
 import { initialProfile } from "@/lib/initial-profile";
+import { db } from "@/lib/db";
+import { redirect } from "next/navigation";
 
 const SetupPage = async () => {
     const profile = await initialProfile(); 
-    return 
-    <div>
-        Create a server!
-    </div>;
+    const server = await db.server.findFirst({
+        where: {
+          members: {
+            some: {
+              profileId: profile.id
+            }
+          }
+        }
+      }
+    );
+
+      if (server) return redirect(`/servers/${server.id}`);
+
+      return <InitialModal />;
 }
  
 export default ; 
