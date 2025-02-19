cb0c9d94a048817d32cafe3d6978d72399f6a514
abransh <abranshbaliyan2807@gmail.com>
2025-02-20 00:51:05 +0100

    file-upload update

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -2,6 +2,10 @@
 
 import { UploadDropzone } from "@/lib/uploadthing"; 
 import "@uploadthing/react/styles.css";
+import { FileIcon, X } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
 
 interface FileUploadProps {
     onChange: (url?: string) => void;
@@ -9,6 +13,28 @@ interface FileUploadProps {
     endpoint: "messageFile" | "serverImage";
 } 
 export const FileUpload: React.FC<FileUploadProps> = ({ onChange, value, endpoint }) => {
+    const fileType = value?.split(".").pop();
+
+    if (value && fileType !== "pdf") {
+        return (
+          <div className="relative h-20 w-20">
+            <Image
+             fill 
+            src={value}
+            alt="Upload" 
+            className="rounded-full" 
+            />
+            <button
+              onClick={() => onChange("")}
+              className="bg-rose-500 text-white p-1 rounded-full absolute top-0 right-0 shadow-sm"
+              type="button"
+            >
+              <X className="h-4 w-4" />
+            </button>
+          </div>
+        );
+      }
+    
     return (
         <UploadDropzone
         endpoint={endpoint}
