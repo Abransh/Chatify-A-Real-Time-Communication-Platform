ba600bc36ca4417d85934e3d7980ef0823c9becf
abransh <abranshbaliyan2807@gmail.com>
2025-02-20 03:03:05 +0100

    NAVIGATION SIDEBAR

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -8,7 +8,12 @@ const MainLayout = async(
 ) => {
     return (  
         <div className="h-full">
+            <div className="Hidden sm:flex h-full w-[72px] z-30 flex-col fixed inset-y-0"> 
+
+            </div>
+            <main className="md:pl-[72px] h-full">
             {children}
+            </main>
            </div>
     );
 }
