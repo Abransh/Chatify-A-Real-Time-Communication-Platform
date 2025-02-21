71d1be757a1b1197da75f1ef2874772d229172b0
abransh <abranshbaliyan2807@gmail.com>
2025-02-21 01:57:56 +0100

    creating server sidebar update

--- app/(main)/(routes)/servers/[serverId]/layout.tsx
+++ app/(main)/(routes)/servers/[serverId]/layout.tsx
@@ -1,24 +1,77 @@
+// import React from "react";
+// import { RedirectToSignIn } from "@clerk/nextjs";
+// import { redirect } from "next/navigation";
+
+// import { currentProfile } from "@/lib/current-profile";
+// import { db } from "@/lib/db";
+// import ServerSidebar from "@/components/server/server-sidebar";
+
+// const ServerIdLayout = async  ({
+//   children,
+//   params,
+// }: {
+//   children: React.ReactNode;
+//   params: { serverId: string };
+// }) => {
+//   const profile = await currentProfile();
+
+//   if (!profile) {
+//     return <RedirectToSignIn redirectUrl="/sign-in" />;   //check this   earlier it could be if (!profile) return redirectToSignIn();
+//   }
+  
+//   const server = await db.server.findUnique({
+//     where: {
+//       id: params.serverId,
+//       members: {
+//         some: {
+//           profileId: profile.id
+//         }
+//       }
+//     }
+//   });
+
+//   if (!server) {
+//     return redirect("/");
+//   }
+  
+//   return (
+//     <div className="h-full">
+//       <div className="hidden md:flex h-full w-60 z-20 flex-col fixed inset-y-0">
+//        {/* <ServerSidebar />  */}
+//        Server Sidebar
+//       </div>
+//       <main className="h-full md:pl-60">
+//         {children}
+//         </main>
+//     </div> 
+//   );
+// }
+
+// export default ServerIdLayout;
+
+
 import React from "react";
-import { RedirectToSignIn } from "@clerk/nextjs";
+import { RedirectToSignIn, RedirectToSignUp } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
+import { auth } from "@clerk/nextjs/server";
 
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import ServerSidebar from "@/components/server/server-sidebar";
+//import { ServerSidebar } from "@/components/server/server-sidebar";
 
-const ServerIdLayout = async  ({
+export default async function ServerIdLayout({
   children,
-  params,
+  params
 }: {
   children: React.ReactNode;
   params: { serverId: string };
-}) => {
+}) {
   const profile = await currentProfile();
 
-  if (!profile) {
-    return <RedirectToSignIn redirectUrl="/sign-in" />;   //check this   earlier it could be if (!profile) return redirectToSignIn();
-  }
+  if (!profile) return <RedirectToSignUp redirectUrl="/sign-up" />;
   
+
   const server = await db.server.findUnique({
     where: {
       id: params.serverId,
@@ -30,21 +83,15 @@ const ServerIdLayout = async  ({
     }
   });
 
-  if (!server) {
-    return redirect("/");
-  }
-  
+  if (!server) return redirect("/");
+
   return (
     <div className="h-full">
-      <div className="hidden md:flex h-full w-60 z-20 flex-col fixed inset-y-0">
-       {/* <ServerSidebar />  */}
-       Server Sidebar
+      <div className="flex h-full w-60 z-20 flex-col fixed inset-y-0">
+       server sidebar
+        <ServerSidebar serverId={params.serverId} />
       </div>
-      <main className="h-full md:pl-60">
-        {children}
-        </main>
-    </div> 
+      <main className="h-full md:pl-60">{children}</main>
+    </div>
   );
 }
-
-export default ServerIdLayout;
