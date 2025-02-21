8cfe353a2ba897ec301abd5bae65286c1404655f
abransh <abranshbaliyan2807@gmail.com>
2025-02-21 02:32:18 +0100

    creating server header update

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -5,12 +5,15 @@ import { MemberRole } from "@prisma/client";
 import { DropdownMenu,
          DropdownMenuContent,
          DropdownMenuItem, 
+         DropdownMenuSeparator, 
          DropdownMenuTrigger } 
          from "@/components/ui/dropdown-menu";
 
 import { ChevronDown,
+        LogOut,
         PlusCircle,
         Settings, 
+         Trash, 
          UserPlus,
          Users
         } from "lucide-react";
@@ -79,6 +82,28 @@ interface ServerHeaderProps {
             <PlusCircle className="h-4 w-4 ml-auto" />
           </DropdownMenuItem>
         )}
+        {isModerator && (
+        <DropdownMenuSeparator /> 
+      )}
+      {isAdmin && (
+          <DropdownMenuItem
+           // onClick={() => onOpen("editServer", { server })}
+            className="text-rose-500 px-3 py-2 text-sm cursor-pointer"
+          >
+            Delete Server
+            <Trash className="h-4 w-4 ml-auto" />
+          </DropdownMenuItem>
+      )}
+
+          {!isAdmin && (
+          <DropdownMenuItem
+           // onClick={() => onOpen("editServer", { server })}
+            className="text-rose-500 px-3 py-2 text-sm cursor-pointer"
+          >
+            Leave Server
+            <LogOut className="h-4 w-4 ml-auto" />
+          </DropdownMenuItem>
+      )}  
         </DropdownMenuContent>
 
         
