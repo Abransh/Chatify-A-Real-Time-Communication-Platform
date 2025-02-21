8ee3a2707e93ca72a802459bc49f9e2138193318
abransh <abranshbaliyan2807@gmail.com>
2025-02-21 03:29:51 +0100

    an api for server routing

--- app/api/servers/[serverId]/invite-code/route.ts
+++ app/api/servers/[serverId]/invite-code/route.ts
@@ -1,4 +1,5 @@
 import { NextResponse } from "next/server";
+import { v4 as uuidv4 } from "uuid";
 
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
@@ -11,11 +12,20 @@ export async function PATCH(
     const profile = await currentProfile();
     const { name, imageUrl } = await req.json();
 
-    if (!profile) return new NextResponse("Unauthorized", { status: 401 });
+    if (!profile)
+ return new NextResponse("Unauthorized", { status: 401 });
+    if (!params.serverId)
+    return new NextResponse("Server ID is missing", { status: 400 });
+
 
     const server = await db.server.update({
-      where: { id: params.serverId, profileId: profile.id },
-      data: { name, imageUrl }
+      where: { 
+        id: params.serverId, 
+     profileId: profile.id },
+      data:
+       { 
+        inviteCode: uuidv4(),
+    },
     });
 
     return NextResponse.json(server);
