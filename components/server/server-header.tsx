d3c58d8c25215a0d19477424c979a7d78c6983d5
abransh <abranshbaliyan2807@gmail.com>
2025-02-25 18:00:48 +0100

    Leave server

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -89,7 +89,7 @@ interface ServerHeaderProps {
       )}
       {isAdmin && (
           <DropdownMenuItem
-            onClick={() => onOpen("editServer", { server })}
+            onClick={() => onOpen("leaveServer", { server })}
             className="text-rose-500 px-3 py-2 text-sm cursor-pointer"
           >
             Delete Server
