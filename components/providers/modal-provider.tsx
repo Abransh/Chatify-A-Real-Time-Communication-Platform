1efb190a51fc6c68808cf7726e663eeee01f85da
abransh <abranshbaliyan2807@gmail.com>
2025-02-25 11:25:42 +0100

    Editing server setting

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -3,6 +3,7 @@
 import { CreateServerModal } from "@/components/modals/create-server-modal";
 import { useEffect, useState } from "react";
 import { InviteModal } from "@/components/modals/invite-modal";
+import { EditServerModal } from "@/components/modals/edit-server-modal";
 
 export const ModalProvider: React.FC = () => {
     const [isMounted, setIsMounted] = useState(false);
@@ -10,11 +11,12 @@ export const ModalProvider: React.FC = () => {
     useEffect(() => {
         setIsMounted(true);
       }, []);
-
+ 
   return (
     <>
       <CreateServerModal />
       <InviteModal />
+      <EditServerModal />
      
     </>
   );
