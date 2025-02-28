331f2813d04030f721f7c98996d926b3547138af
abransh <abranshbaliyan2807@gmail.com>
2025-03-01 00:41:22 +0100

    Delete server done

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -89,7 +89,7 @@ interface ServerHeaderProps {
       )}
       {isAdmin && (
           <DropdownMenuItem
-            onClick={() => onOpen("leaveServer", { server })}
+            onClick={() => onOpen("deleteServer", { server })}
             className="text-rose-500 px-3 py-2 text-sm cursor-pointer"
           >
             Delete Server
