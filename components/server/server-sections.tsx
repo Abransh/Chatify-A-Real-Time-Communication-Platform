721ea0c113532a2c4ecb0c68d641e094b2edbf77
abransh <abranshbaliyan2807@gmail.com>
2025-03-01 01:38:17 +0100

    Server Selection update

--- components/server/server-sections.tsx
+++ components/server/server-sections.tsx
@@ -26,8 +26,16 @@ interface ServerSectionProps {
    
 } ServerSectionProps) => {
     return ( 
-        <div>
-            Server Section
+        <div className="flex items-center justify-between py-2">
+      <p className="text-xs uppercase font-semibold text-zinc-500 dark:text-zinc-400">
+        {label}
+      </p>
+      {role !== MemberRole.GUEST && sectionType === "channels" && (
+        <ActionTooltip label="Create Channel" side="top">
+          <button
+            onClick={() => onOpen("createChannel", { channelType })}
+            className="text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition"
+          ></button>
         </div>
      );
   }
