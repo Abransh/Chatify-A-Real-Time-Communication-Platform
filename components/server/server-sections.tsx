1f6ccd2360d980d05261ce91d23ef753a058b037
abransh <abranshbaliyan2807@gmail.com>
2025-03-01 01:44:00 +0100

    Server Selection update

--- components/server/server-sections.tsx
+++ components/server/server-sections.tsx
@@ -6,7 +6,7 @@ import { Plus, Settings } from "lucide-react";
 
 import { ServerWithMembersWithProfiles } from "@/types";
 import { ActionTooltip } from "@/components/action-tooltip";
-import { useModal } from "@/hooks/use-modal-store";
+import { useModal } from "@/components/hooks/use-modal-store";
 
 interface ServerSectionProps {
     label: string;
@@ -24,10 +24,13 @@ interface ServerSectionProps {
   role,
   server, 
    
-} ServerSectionProps) => {
+}: ServerSectionProps) => {
+
+    const { onOpen } = useModal();
+
     return ( 
         <div className="flex items-center justify-between py-2">
-      <p className="text-xs uppercase font-semibold text-zinc-500 dark:text-zinc-400">
+      <p className="text-xs upperca  se font-semibold text-zinc-500 dark:text-zinc-400">
         {label}
       </p>
       {role !== MemberRole.GUEST && sectionType === "channels" && (
@@ -35,7 +38,11 @@ interface ServerSectionProps {
           <button
             onClick={() => onOpen("createChannel", { channelType })}
             className="text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition"
-          ></button>
+              >
+                <Plus className="w-4 h-4" />
+              </button>
+            </ActionTooltip>
+          )}
         </div>
      );
   }
