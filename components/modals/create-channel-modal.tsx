6b7025db5080a22f1e2b00674b2f73d275f368be
abransh <abranshbaliyan2807@gmail.com>
2025-03-03 12:54:08 +0100

    server channel update

--- components/modals/create-channel-modal.tsx
+++ components/modals/create-channel-modal.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { use } from "react";
+import React, { use, useEffect } from "react";
 import {
   Dialog,
   DialogContent,
@@ -54,19 +54,32 @@ const formSchema = z.object({
 });
 
 export const CreateChannelModal: React.FC = () => {
-    const { isOpen, onClose, type } = useModal();
+    const { isOpen, onClose, type, data } = useModal();
     const router = useRouter();
     const params = useParams();
 
     const isModalOpen = isOpen && type === "createChannel";
+
+    const { channelType } = data;
      
     const form = useForm({
       resolver: zodResolver(formSchema),
       defaultValues: {
         name: "",
-        type: ChannelType.TEXT   ,  //channelType || ChannelType.TEXT
+        type: channelType || ChannelType.TEXT   ,  //channelType || ChannelType.TEXT
       }
     });
+
+    useEffect (() => {
+        if(channelType) {
+          form.setValue("type", channelType);
+        } 
+        else{
+          form.setValue("type", ChannelType.TEXT);
+        }
+
+    }, [channelType, form]
+  ); 
 
     const isLoading = form.formState.isSubmitting;
 
