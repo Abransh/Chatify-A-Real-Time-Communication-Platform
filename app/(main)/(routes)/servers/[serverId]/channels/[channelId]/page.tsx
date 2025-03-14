8837ec02d2a1554969f6fb446efd3d0ea01dd9f2
abransh <abranshbaliyan2807@gmail.com>
2025-03-14 20:06:33 +0100

    making chat header

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,9 +1,37 @@
- const ChannelIdPage = () => {
-    return ( 
-        <div>
-            Channel Id Page!
-        </div>
-     );
- }
+import React from "react";
+import { RedirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { ChannelType } from "@prisma/client"; 
+ 
+interface ChannelIdPageProps {
+    params: {
+      serverId: string;
+      channelId: string;
+    };
+  }
   
- export default ChannelIdPage;
+  export default async function ChannelIdPage({
+    params: { channelId, serverId }
+  }: ChannelIdPageProps) {
+    const profile = await currentProfile();
+  
+    if (!profile) return redirectToSignIn();
+  
+    const channel = await db.channel.findUnique({
+      where: { id: channelId }
+    });
+  
+    const member = await db.member.findFirst({
+      where: { serverId: serverId, profileId: profile.id }
+    });
+
+
+    if (!channel || !member) return redirect("/");
+
+    return (
+      <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
+        <ChatHeader
+          name={channel.name}
+          serverId={channel.serverId}
+          type="channel"
+        />
